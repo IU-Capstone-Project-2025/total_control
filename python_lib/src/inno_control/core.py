5c677fc5d25899b8dbd0dc5a0f8e27a5ce8657f2
Evgenii Shlomov <e.shlomov@innopolis.university>
2025-06-11 13:13:12 +0300

    chore: main repo structure

---
+++ python_lib/src/inno_control/core.py
@@ -0,0 +1,5 @@
+import os
+import sys
+import serial
+
+print('Someday something smart will appear here, but for now it was simply able to import the Serial library')
---
+++ scripts/get_docs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# --- Settings ---
+DOCS_DIR="docs"          # Docs path
+PORT=8000                # Local server port
+
+## --- Check folder ---
+#if [ ! -d "$DOCS_DIR" ]; then
+# echo "Error: Documentation folder '$DOCS_DIR' not found!"
+#  exit 1
+#fi
+
+#cd "$DOCS_DIR" || exit 1
+
+## --- Build HTML ---
+echo "Building html..."
+#if ! make html; then
+#  echo "Error during bilding!"
+#  exit 1
+#fi
+
+## --- Start server ---
+echo "Starting server at http://localhost:$PORT"
+echo "Press Ctrl+C to stop"
+
+python3 -m http.server --directory ./docs/build/html "$PORT"
