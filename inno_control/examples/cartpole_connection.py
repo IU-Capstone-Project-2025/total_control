eac8cd00aeabc8da0f899db4130375e37ac9050e
StasiaNenastie <148863747+StasiaNenastie@users.noreply.github.com>
2025-06-25 23:47:32 +0300

    feat: add simple controller

--- inno_control/examples/cartpole_connection.py
+++ inno_control/examples/cartpole_connection.py
@@ -2,4 +2,22 @@ from inno_control.devices import CartPole
 
 a = CartPole('/dev/cu.usbserial-110')
 print('Начало положено...')
-# a.connect()
+a.connect()
+a.start_experimnet()
+
+max_force = 90
+centre = 6251
+input()
+while True:
+    try:
+        res = a.get_joint_state().split()
+        print(res)
+        coef = centre - int(res[0])
+        force = (coef / abs(coef)) * 40 + 0.01 * coef
+        if(abs(force)>max_force):
+            force = max_force
+        print(force)
+        a.set_joint_efforts(force)
+    except (Exception) as e:
+        print(e)
+        continue
