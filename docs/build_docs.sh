9abf99a8b3136718d9096ef1a1e004e87b4949a0
Artyom35689 <artom.tuzov@mail.ru>
2025-06-30 16:25:01 +0300

    ci: add documentation deploy

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -1,6 +1,3 @@
-cd .. 
-pip install -e inno_control/
-cd docs
-rm -rf docs/*
+pip install -e ../inno_control/
 sphinx-build -M html . ./build -v -a -E
 python3 -m http.server --directory ./build/html 8000
